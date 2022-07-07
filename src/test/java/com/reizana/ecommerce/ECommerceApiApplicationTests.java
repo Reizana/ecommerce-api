f5e6ae3495f94e4f2ece10cb1ace7dbc429361a4
Luís Reinaldo Zanatto <luisreinaldozanatto@gmail.com>
2022-07-06 22:18:34 -0300

    Initial commit

---
+++ src/test/java/com/reizana/ecommerce/ECommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reizana.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
